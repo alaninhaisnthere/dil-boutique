82f0eac291bb3ab07af24befbf0e9a8a187b2c50
Alana Oliveira <alanaoliveira71@gmail.com>
2023-12-27 22:31:41 -0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    content: [
+      "./src/**/*.html",
+      "./src/**/*.tsx",
+      "./src/**/*.ts",
+      "./src/**/*.js",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
